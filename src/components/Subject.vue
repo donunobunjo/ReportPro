1308fe1c25223a5be0caf3ed49feab019510b237
donunobunjo <don.unobunjo@gmail.com>
2020-06-24 22:35:28 +0100

    Feature:implemeted delete for subjects

--- src/components/Subject.vue
+++ src/components/Subject.vue
@@ -9,38 +9,36 @@
                     <form id="frm">
                         <div class="row form-group">
                             <label for="subject">Subject</label>
-                            <input type="text" id="subject" class="form-control frminput" placeholder="New subject ..." v-model='subject.subject'>
+                            <input type="text" id="subject" class="form-control frminput" placeholder="New subject ..." v-model='subject.subject' ref="subj">
                             <span v-if="error.errSubject" class="err">{{error.errSubject}}</span>
                         </div>
                         <div class="form-group">
-                            <input type="button" value="Add" @click="add" class="btn btn-primary float-right">
-                            
+                            <!-- <input type="button" value="Add" @click="add" class="btn btn-primary float-right"> -->
+                            <el-row>
+                                <b-spinner type="grow" label="Busy" v-if="spinner"></b-spinner>
+                                <el-button type="success" @click="add" icon="el-icon-check" circle class="float-right">Add</el-button>
+                            </el-row>>
                         </div>
                     </form>
                     <hr>
                     <div class="tbl table-responsive">
                         <table class="table table-striped">
                             <thead>
-                                <tr>
+                                <tr style="text-align:center;">
                                     <th>
-                                        Subjects
+                                        Available Subjects
                                     </th>
+                                    <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
+                                <tr v-for="subject in subjects" :key='subject.id'>
                                     <td>
-                                        Physics
+                                        {{subject.subject}}
                                     </td>
-                                </tr>
-                                <tr>
                                     <td>
-                                        Economics
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        Statistics
+                                        <el-button type="primary" icon="el-icon-edit" circle></el-button>
+                                        <el-button type="danger" icon="el-icon-delete" circle  @click="destroy(subject)"></el-button>
                                     </td>
                                 </tr>
                             </tbody>
@@ -53,7 +51,8 @@
 </template>
 
 <script>
- import baseurl from './baseURL';
+import baseurl from './baseURL';
+import swal from 'sweetalert';
 export default {
     data () {
         return {
@@ -63,25 +62,63 @@ export default {
             error:{
                 errSubject:''
             },
-            subjects:[]
+            subjects:[],
+            spinner:false
         }
     },
     methods:{
         add(){
-            // console.log('hahahahah')
-            if(this.subject==''){
+            if(this.subject.subject==''){
                 this.error.errSubject='Enter a subject'
                 setTimeout(()=>{
                     this.error.errSubject=''
                 },4000)
                 return false 
             }
-            //console.log(this.subject)
+            this.spinner=true
             this.axios.post(baseurl+'/subject',this.subject)
             .then((res)=>{
-                console.log(res)
+                this.subjects.splice(0,0,res.data.subject)
+                this.subject.subject=''
+                this.spinner=false
+                this.$refs.subj.focus()
+                
             })
+        },
+        destroy(subject){
+            swal({
+                title: "Are you sure?",
+                text: "Once deleted, you will not be able to recover this subject!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+                })
+                .then((willDelete) => {
+                if (willDelete) {
+                    this.axios.delete(baseurl+'/subject/'+subject.id)
+                    .then((res)=>{
+                       if (res.data.message=='Subject Deleted successfully'){
+                         this.subjects.splice(this.subjects.indexOf(subject), 1);
+                        }
+                    })
+                    swal("Subject deleted successfully!", {
+                    icon: "success",
+                    });
+                } else {
+                    //swal("Your imaginary file is safe!");
+                }
+            });
+
         }
+    },
+    mounted(){
+        this.axios.get(baseurl+'/subject')
+        .then((res)=>{
+            this.subjects = res.data.subjects
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
     }
     
 }
--- src/main.js
+++ src/main.js
@@ -3,15 +3,19 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 import routes from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueAxios from 'vue-axios'
 import Axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 //import baseurl from 'baseURL'
 Vue.use(VueAxios,Axios)
 Vue.use(ElementUI)
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 const router = new VueRouter({
   mode:'history',
   routes:routes
